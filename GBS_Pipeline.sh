793fbafb340c7dd05aa9a067ded6efb91f0f16f9
Paulo <pahenrique02@gmail.com>
2025-09-04 18:24:29 -0300

    Versionando o diretório

---
+++ GBS_Pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author: PHSS
+# Date: 04/09/2025
+
+echo "I am at:" $PWD
+echo "First, cloning Tassel4Poly repository"
+
+# git clone https://github.com/guilherme-pereira/tassel4-poly.git
+
+# Add to the PATH to avoid need to use absolute path
+export PATH=$PATH:$HOME/Documentos/DataScience/BackupGBS_ServerUNESP/tassel4-poly
+
+# Creating the required directories for the analysis
+mkdir -p topm tbt tagCounts reference mergedTagCounts mergedTBT logs hapmap fastq barcodes
+
+# Place your reference genome at reference folder, your barcode at barcodes folder 
+# and in the fastq folder with your fastq files using the flowcell name and lanes 
+# with the extension _fastq.txt.gz.
+
+run_pipeline.pl -Xmx12g -fork1 -FastqToTagCountPlugin \
+  -i ./fastq \
+  -k ./barcodes/Key161.txt \
+  -e PstI-MseI -c 1 \
+  -o tagCounts \
+  -endPlugin -runfork1 | tee ./logs/VCFCallingstep01.log.txt
+#teste
